aea33168682fcf0cb9041f249255f4d575375998
sforesi <sebastiano.foresi@gmail.com>
2021-12-06 18:41:56 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -10,8 +10,37 @@ export default function App() {
   const [products, setProducts] = useState(productsArr);
   const [cart, setCart] = useState([]);
 
+  const AllTheThingscart = productsArr.map((ele, index) => {
+    return (
+      <ShoppingItems
+      name={ele,name}
+      price={ele.price}
+      key={index}
+      handleClick={() => removeFromCart(index)}
+      />
+    )
+  }) 
+
+
+  const MyShoppingCart = cart.map((ele, index) => {
+    return (
+      <ShoppingItems
+      name={ele,name}
+      price={ele.price}
+      key={index}
+      handleClick={() => removeFromCart(index)}
+      />
+    )
+  }) 
   // create an addToCart function that takes in a product as a param
   // using the ...spread operator add the product to the cart array
+
+  function removeFromCart(index) {
+    let newShoppingCart = cart.filter((item, idx) => {
+      return index !== index
+    })
+    setCart(newShoppingCart)
+  }
 
   // create an removeFromCart function that takes in an index as a param
   // using Array.filter remove create a new array where that item is removed
